7af951a45691b5b583a3d213558bea7f4c1d727d
BuickSalesman <adam.odonnell.e@gmail.com>
2025-08-25 18:10:50 -0400

    added index.html

--- main.js
+++ main.js
@@ -18,4 +18,51 @@ const createWindow = () => {
 
     overlay.loadFile("index.html")
     overlay.setIgnoreMouseEvents(true, { forward: true });
-}  
+    //overlay.webContents.openDevTools({ mode: "detach" });
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    //stop tracking when leaving desktop space
+    setInterval(() => trackWindows(overlay), 30)
+    /*
+     * @trackWindows will need to be renamed to something like updateWindows
+     * as it will also link to hammerspoon updating coordinates from the physics engine    
+     */
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//activate means on mac when a process is running but there are no active windows, and you click the icon in the dock.
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
