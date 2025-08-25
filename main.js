8a30d34cdbcfa2ab795fcdaa9d45e6aa2732cd57
BuickSalesman <adam.odonnell.e@gmail.com>
2025-08-25 18:08:43 -0400

    started scaffolding electron window

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Electron!)
+const { BrowserWindow, screen, app } = require("electron")
+
+
+
+const createWindow = () => {
+    const { x, y, width, height } = screen.getPrimaryDisplay().bounds
+
+    const overlay = new BrowserWindow({
+        x,
+        y,
+        width,
+        height,
+        transparent: true,
+        alwaysOnTop: true,
+        focusable: false,
+        hasShadow: false,
+    });
+
+    overlay.loadFile("index.html")
+    overlay.setIgnoreMouseEvents(true, { forward: true });
+}  
